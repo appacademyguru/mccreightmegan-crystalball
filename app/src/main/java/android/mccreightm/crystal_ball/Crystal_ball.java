b93f50eac018a1ecfdf162721335f305b66257a0
Megan McCreight <appacademy.megan@gmail.com>
2014-09-16 14:37:54 -0700

    Milestone 19: initializing the acceleration variables

--- app/src/main/java/android/mccreightm/crystal_ball/Crystal_ball.java
+++ app/src/main/java/android/mccreightm/crystal_ball/Crystal_ball.java
@@ -38,6 +38,9 @@ public class Crystal_ball extends Activity {
 
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        acceleration = 0.0f;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        previousAcceleration = SensorManager.GRAVITY_EARTH;
 
         answerText = (TextView) findViewById(R.id.answerText);
         answerText.setText(Predictions.get().getPrediction());
